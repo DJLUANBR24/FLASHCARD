51fecc9497990635779fdcec2330ea55300e0eed
LUAN ROMERO AMORIM <luanamorim2406@gmail.com>
2024-11-19 12:11:43 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,41 @@
+criaCartao(
+    'Programação',
+    'O que é Python ?',
+    'O Python é uma linguagem de programação'
+)
+
+criaCartao(
+    'Geografia',
+    'Qual a capital da França ?',
+    'A capital da França é Paris'
+)
+
+criaCartao(
+    'Programação',
+    'O que é uma função ?',
+    'Uma função é um bloco de código que executa alguma tarefa'
+)
+
+criaCartao(
+    'Lingua inglesa',
+    'Como se diz oi em Inglês ?',
+    'Oi em ingles é HI (RAI)'
+)
+
+criaCartao(
+    'Cinema',
+    'Quem é o diretor do filme "Tubarão" de 1975 ?',
+    'Steven Spielberg'
+)
+
+criaCartao(
+    'Video-game',
+    'Qual o nome do protagonista da franquia de jogos "God of War" ?',
+    'Kratos, também conhecido como "Fantasma de Esparta"'
+)
+
+criaCartao(
+    'História',
+    'Quem ordenou o ataque às torres gêmeas nos Estados Unidos em 2001 ?',
+    'O terrorista Osama bin Laden, líder do grupo terrorista Al Qaeda'
+)
